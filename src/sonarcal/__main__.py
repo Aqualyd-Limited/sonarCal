4048e31dc2e3ea4b7be3c20e8978238945d37329
Gavin Macaulay <gavin@macaulay.co.nz>
2025-10-31 15:43:18 +1300

    More package setup stuff

---
+++ src/sonarcal/__main__.py
@@ -0,0 +1,4 @@
+
+if __name__ == "__main__":
+    import sonarcal
+    print('Running via runpy!')
---
+++ src/sonarcal/sonar_cal.py
@@ -0,0 +1,4 @@
+import sonarcal
+
+if __name__ == "__main__":
+    print('sonarcal.py run as main')
