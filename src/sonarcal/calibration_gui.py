f16e47a363b4db0814d87bc6e338438f96f1c670
Gavin Macaulay <gavin@macaulay.co.nz>
2025-11-12 14:34:45 +1300

    Move assets dir to be present in installed packages

--- src/sonarcal/calibration_gui.py
+++ src/sonarcal/calibration_gui.py
@@ -16,7 +16,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from platformdirs import PlatformDirs
 
 logger = logging.getLogger(app_name)
-icon_file = Path(__file__).parent.parent.parent/'assets'/'icon.png'  # TODO get via a config file
+icon_file = Path(__file__).parent/'assets'/'icon.png'  # TODO get via a config file
 
 class calibrationGUI:
     """Provides the main GUI container and misc labels/buttons."""
