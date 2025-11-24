c414d7cc81bace864daadea46708dac3576f0013
Gavin Macaulay <gavin@macaulay.co.nz>
2025-11-24 15:10:52 +1300

    Tidy up main() some more

--- src/sonarcal/controller.py
+++ src/sonarcal/controller.py
@@ -1,7 +1,7 @@
 """Omnisonar calibration program
 
 Provides omni and echogram displays and sphere amplitude plots for use when
-calibrating omni-directional sonars.
+calibrating sonars with horizontal beams.
 """
 
 import tkinter as tk
@@ -14,13 +14,8 @@ from .echogram_plotter import echogramPlotter
 from .utils import setupLogging, on_exit, window_closed
 from .file_ops import sonar_file_read
 from .calibration_gui import calibrationGUI
-from .configuration import config
-
-if sys.platform == "win32":
-    import win32api
 
 setupLogging()
-
 
 def main():
     """Omnisonar calibration graphical user interface."""    
@@ -33,7 +28,7 @@ def main():
     echogram = echogramPlotter(msg_queue, root)
     gui = calibrationGUI(echogram)
 
-    # Start reading of sonar files in a separate thread
+    # Sonar files are read from a separate thread
     t = threading.Thread(target=sonar_file_read, args=(msg_queue,))
 
     t.daemon = True  # makes the thread close when main() ends
@@ -41,10 +36,9 @@ def main():
 
     # For Windows, catch when the console is closed
     if sys.platform == "win32":
+        import win32api
         win32api.SetConsoleCtrlHandler(partial(on_exit, gui.root(), gui.job()), True)
 
     # And start things...
     root.protocol("WM_DELETE_WINDOW", lambda: window_closed(gui.root(), gui.job()))
     root.mainloop()
-
-
