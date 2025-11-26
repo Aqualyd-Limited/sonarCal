Fix bugs in tilt calculation and coding. May also fix incorrect TS and Sv values

src/sonarcal/datagram_processor.py
@@ -212,8 +212,9 @@ class rawDatagramProcessor():
 
         # TODO - check that the direction of these angles is as required by the polar plot
         # - first glance suggests it is not!
+        
         self.theta = np.array(th)
-        self.tilts = np.array(tl)
+        self.tilts = np.pi/2 - np.array(tl)
         self.labels = np.array(lbls)
         self.gain_rx = np.array(g)
         self.gain_adjust = np.array(g_a)

src/sonarcal/echogram_plotter.py
@@ -256,8 +256,12 @@ class echogramPlotter:
         self.fig.canvas.draw_idle()
 
     def updateTiltValue(self, tilt: float):
+        """Update the display of beam tilt on GUI.
+        
+        Units of tilt are radians
+        """
         if self.gui_created:
-            self.beamTiltText.set_text(f'Tilt: {tilt:0.1f}°')
+            self.beamTiltText.set_text(f'Tilt: {tilt*180/np.pi:0.1f}°')
 
     def updateDiffPlotYLim(self):
         """Update the lower limit of the difference plot y-axis."""
@@ -425,6 +429,7 @@ class echogramPlotter:
                                                axis=0)
 
                     self.polarPlot.set_array(self.polar.ravel())
+
                     self.updateTiltValue(tilts[self.beamIdx])
 
                     if self.new_ping_cb: