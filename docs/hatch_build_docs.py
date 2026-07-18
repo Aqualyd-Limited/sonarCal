0c0cd687ba63d58af9defe64694c3c5b0bf6725d
Gavin Macaulay <gavin@macaulay.co.nz>
2026-07-18 16:44:41 +1200

    cope with different Windows/Ubuntu subprocess.run requirements

--- docs/hatch_build_docs.py
+++ docs/hatch_build_docs.py
@@ -1,11 +1,14 @@
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from os import environ
+import platform
 from subprocess import run
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         """Run zensical build in an OS independent way."""
 
+        shell = True if platform.system() == "Windows" else False
+
         run(['uv', 'tool', 'run', 'zensical', 'build'], 
-            shell=True,
+            shell=shell,
             env=environ | {'OFFLINE_PLUGIN_ENABLED': 'true'})
